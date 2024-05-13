76f3d3ee4ee4d4583e0c08fd1d2067f0164bdf6f
kingzturch <163362090+kingzturch@users.noreply.github.com>
2024-05-13 14:46:58 +0700

    update all

---
+++ login.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Webleb</title>
+  <link href="styles.css" rel="stylesheet" type="text/css" />
+  <script src="script.js" defer></script>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container">
+      
+          <div class="navbar-header">
+            <button class="navbar-toggler" data-toggle="open-navbar1">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <a href="#">
+              <h4>Sistem Informasi<p></p><span>Pemantauan Kualitas Air Bersih</span></h4>
+            </a>
+          </div>
+      
+          <div class="navbar-menu" id="open-navbar1">
+            <ul class="navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li class="navbar-dropdown">
+                <a href="#" class="dropdown-toggler" data-dropdown="my-dropdown-id">
+                  Login <i class="fa fa-angle-down"></i>
+                </a>
+                <ul class="dropdown" id="my-dropdown-id">
+                  <li><a href="user.php">USER</a></li>
+                  <li class="separator"></li>
+                  <li><a href="admin.php">ADMIN</a></li>
+                  <li class="separator"></li>
+                  <li><a href="pegawailapangan.php">Pegawai lapangan</a></li>
+                </ul>
+              </li>
+           
+              <li><a href="#">About</a></li>
+              
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+</body>
+</html>
